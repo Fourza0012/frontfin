8a4fbf6a7f827537f0e530dcfcd769783207ff28
Fourza0012 <kittichat.tun@gmail.com>
2023-02-14 21:20:22 +0700

    [FEAT] ethers: update version and add erc20 abi

--- hooks/ethereum.ts
+++ hooks/ethereum.ts
@@ -1,5 +1,4 @@
 import { ethers } from "ethers";
-
 export const useEthereum = () => {
     const handleMetaLogin = async () => {
         try {
@@ -15,15 +14,15 @@ export const useEthereum = () => {
                 }
             } else {
                 provider = new ethers.BrowserProvider(window.ethereum)
-                console.log('in provider', provider)
                 signer = await provider.getSigner();
                 const address = await signer.getAddress();
+                // let contract = new Contract("0x328371ceF3baF12d62A64215FfBEc0B1Dbb86e60", abi, provider)
+                // console.log('test', await contract.balanceOf('0x3798E24E8B0770cF14d5A98B7e01113d1BA01c51'))
                 return {
                    account: address,
                    provider,
                    signer
                 }
-                // console.log('test', formatEther(await provider.getBalance('0x3798E24E8B0770cF14d5A98B7e01113d1BA01c51')))            
             }
         } catch (e) {
             console.log('some error', e)
